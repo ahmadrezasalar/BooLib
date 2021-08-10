f1205d023b594dad72c7938267474cbf900dfa9d
mamalrms <mamalrms1906@gmail.com>
2021-08-10 16:30:10 -0400

    create BOOK model

--- BooLib/Book/models.py
+++ BooLib/Book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class BOOK(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    isibm = models.CharField(max_length=50,unique=True)
+    price = models.IntegerField()
+
+
